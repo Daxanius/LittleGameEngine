af0faffb41a5edaaeb346d8677dc0043fca55641
Daxanius <balder.huybreghs@gmail.com>
2025-03-09 22:36:45 +0100

    Extend command system

--- Minigin/Command.cpp
+++ Minigin/Command.cpp
@@ -1,1 +1,8 @@
 #include "Command.h"
+
+dae::ActorCommand::ActorCommand(GameObject* actor) : m_Actor(actor) {
+}
+
+dae::GameObject* dae::ActorCommand::GetActor() const {
+	return m_Actor;
+}
--- Minigin/Command.h
+++ Minigin/Command.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "GameObject.h"
 
-class Command {
-public:
-	virtual ~Command() = default;
-	virtual void Execute() = 0;
-};
+namespace dae {
+	class Command {
+	public:
+		virtual ~Command() = default;
+		virtual void Execute() = 0;
+	};
+
+	class ActorCommand : Command {
+	protected:
+		ActorCommand(GameObject* actor);
+
+		GameObject* GetActor() const;
+
+	private:
+		GameObject* m_Actor{ nullptr };
+	};
+}
