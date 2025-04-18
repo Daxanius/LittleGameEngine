f404c85a35f4db2e45a335593ba9912a21d5ed46
Daxanius <balder.huybreghs@gmail.com>
2025-04-18 15:36:06 +0200

    Add SDL mixer

--- Minigin/Minigin.cpp
+++ Minigin/Minigin.cpp
@@ -4,6 +4,7 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #include <thread>
 #include "Minigin.h"
 #include "InputManager.h"
@@ -39,6 +40,14 @@ static void PrintSDLVersion()
 	version = *TTF_Linked_Version();
 	printf("We are linking against SDL_ttf version %u.%u.%u.\n",
 		version.major, version.minor, version.patch);
+
+	SDL_MIXER_VERSION(&version);
+	printf("We compiled against SDL_mixer version %u.%u.%u ...\n",
+					 version.major, version.minor, version.patch);
+
+	version = *Mix_Linked_Version();
+	printf("We are linking against SDL_mixer version %u.%u.%u.\n",
+				 version.major, version.minor, version.patch);
 }
 
 dae::Minigin::Minigin(const std::string &dataPath)
