4b9bf5afd90dee73cfb371e3eb95c7aefdef5320
Daxanius <balder.huybreghs@gmail.com>
2025-05-04 14:52:15 +0200

    Add sprite component

---
+++ Minigin/SpriteComponent.cpp
@@ -0,0 +1,57 @@
+#include "SpriteComponent.h"
+#include "ResourceManager.h"
+#include "Renderer.h"
+
+dae::SpriteComponent::SpriteComponent(GameObject& pOwner, const std::string& texturePath, int frameWidth, int frameHeight, float scale)
+: BaseComponent(pOwner), m_frameWidth(frameWidth), m_frameHeight(frameHeight), m_scale(scale) {
+	m_pTexture = ResourceManager::GetInstance().LoadTexture(texturePath);
+}
+
+void dae::SpriteComponent::Update(float deltaTime) {
+	if (m_states.find(m_currentState) == m_states.end()) return;
+	const State& state = m_states[m_currentState];
+
+	m_elapsedTime += deltaTime;
+
+	float secondsPerFrame = 1.0f / static_cast<float>(state.fps);
+
+	if (m_elapsedTime >= secondsPerFrame) {
+		++m_frame;
+
+		if (m_frame >= state.frames) {
+			m_frame = 0;
+		}
+
+		// allow leftover time to carry over
+		m_elapsedTime -= secondsPerFrame;
+	}
+}
+
+void dae::SpriteComponent::Render() {
+	if (m_states.find(m_currentState) == m_states.end()) return;
+	const auto& worldPosition{ GetOwner().GetWorldTransform().GetPosition() };
+	const auto& state{ m_states[m_currentState] };
+	
+	Renderer::GetInstance().RenderTexture(
+		*m_pTexture,
+		worldPosition.x,
+		worldPosition.y,
+		m_frameWidth * m_scale,
+		m_frameHeight * m_scale,
+		m_frameWidth * state.offset * m_frame,
+		0,
+		m_frameWidth,
+		m_frameHeight
+		);
+}
+
+void dae::SpriteComponent::AddState(StateId stateId, State state) {
+	m_states[stateId] = state;
+}
+
+void dae::SpriteComponent::SetState(StateId stateId) {
+	if (m_states.find(stateId) != m_states.end()) {
+		m_currentState = stateId;
+		m_frame = 0; // Reset the frame when the state changed
+	}
+}
---
+++ Minigin/SpriteComponent.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "BaseComponent.h"
+#include "Texture2D.h"
+#include <memory>
+#include <unordered_map>
+
+namespace dae {
+	using StateId = unsigned int;
+
+	class SpriteComponent : BaseComponent {
+	public:
+		SpriteComponent(GameObject& pOwner, const std::string& texturePath, int frameWidth, int frameHeight, float scale = 1.f);
+
+		struct State {
+			int offset;
+			int frames;
+			int fps;
+		};
+
+		void FixedUpdate() override {};
+		void PostUpdate() override {};
+		void Update(float deltaTime) override;
+		void Render() override;
+
+		void AddState(StateId stateId, State state);
+		void SetState(StateId stateId);
+	private:
+		float m_elapsedTime{};
+		float m_scale{};
+
+		int m_frameWidth{};
+		int m_frameHeight{};
+
+		int m_frame{};
+
+		// The actual texture of the spritesheet
+		std::shared_ptr<Texture2D> m_pTexture;
+		std::unordered_map<StateId, State> m_states;
+
+		StateId m_currentState{};
+	};
+}
